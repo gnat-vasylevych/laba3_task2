fbbed98a16ab9b2bd38c79e49d7fc8137b7b70d1
vlad-is-love <vladyslav.vasylevych@ucu.edu.ua>
2021-02-22 07:39:00 +0200

    only manual navigation logic

--- task_2.py
+++ task_2.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 
 
 def get_file():
@@ -19,26 +20,35 @@ def show_keys(object):
 
 
 def navigation(object):
-    print("Here are the available keys\n")
+    print("\nHere are the available keys\n")
     show_keys(object)
     print("To go a level back type in BACK")
     choice = input("Enter the key or the index: ")
     if choice == "BACK":
         return True
-    if type(object) == list:
-        choice = int(choice)
-    check = type(object[choice])
-    if check == list or check == dict:
-        command = navigation(object[choice])
-        if command:
-            navigation(object)
-    else:
-        print(object[choice])
+    if choice == "SHOW":
+        print()
+        pprint.pprint(object)
+    try:
+        if type(object) == list:
+            choice = int(choice)
+        check = type(object[choice])
+        if check == list or check == dict:
+            command = navigation(object[choice])
+            if command:
+                navigation(object)
+        else:
+            print(object[choice])
+    except (KeyError, ValueError, IndexError):
+        print("\nThere is no such key on this level")
+        print("Type in SHOW to see structure of current level\n")
+        navigation(object)
 
 
 def main():
     data = get_file()
     navigation(data)
+
 
 
 main()
