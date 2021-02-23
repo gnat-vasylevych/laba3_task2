8e53d6976589b7b736169464f77a347a8ce9ac9b
vlad-is-love <vladyslav.vasylevych@ucu.edu.ua>
2021-02-23 13:08:32 +0200

    added requirements, README and doctests

--- task_2.py
+++ task_2.py
@@ -1,48 +1,78 @@
+"""
+This module helps user to navigate through any json file
+"""
 import json
-import pprint
 
 
 def get_file():
-    # file_path = input("Enter the path to file: ")
-    file_path = "frienfs_list_Obama.json"
+    """
+    Reads json file into python dictionary
+    """
+    file_path = input("Enter the path to file: ")
     with open(file_path, encoding='utf-8') as file:
         return json.load(file)
 
 
-def show_keys(object):
-    if type(object) == dict:
-        for key, val in object.items():
+def show_keys(entity):
+    """
+    Shows all the keys of the object if it is dictionary or shows
+    all the elements if it is list
+    >>> show_keys([1, 2, {}])
+    element number 0 is <class 'int'>
+    element number 1 is <class 'int'>
+    element number 2 is <class 'dict'>
+    <BLANKLINE>
+    >>> show_keys({"1": 1, "2": [], "pf": 'yeah'})
+    1 : <class 'int'>
+    2 : <class 'list'>
+    pf : <class 'str'>
+    <BLANKLINE>
+    """
+    if type(entity) == dict:
+        for key, val in entity.items():
             print(key, ':', type(val))
-    elif type(object) == list:
-        for index in range(len(object)):
-            print("element number", index, "is", type(object[index]))
+    elif type(entity) == list:
+        for index in range(len(entity)):
+            print("element number", index, "is", type(entity[index]))
     print()
 
 
-def navigation(object):
-    print("\nHere are the available keys\n")
-    show_keys(object)
-    print("To go a level back type in BACK")
-    choice = input("Enter the key or the index: ")
-    if choice == "BACK":
-        return True
-    if choice == "SHOW":
-        print()
-        pprint.pprint(object)
-    try:
-        if type(object) == list:
-            choice = int(choice)
-        check = type(object[choice])
-        if check == list or check == dict:
-            command = navigation(object[choice])
-            if command:
-                navigation(object)
+def navigation(entity):
+    """
+    Main function for navigation. Shows the keys on the current level and user chooses the key or moves on the previous
+    level (command BACK)
+    """
+    while True:
+        print("\nHere are the available keys\n")
+        show_keys(entity)
+        print("To go a level back type in BACK")
+        choice = input("Enter the key or the index: ")
+        if choice == "BACK":
+            return True
         else:
-            print(object[choice])
-    except (KeyError, ValueError, IndexError):
-        print("\nThere is no such key on this level")
-        print("Type in SHOW to see structure of current level\n")
-        navigation(object)
+            if type(entity) == list:
+                try:
+                    entity[int(choice)]
+                except (ValueError, IndexError):
+                    print("\nThere is no such key on this level\n")
+                    continue
+            elif type(entity) == dict:
+                try:
+                    entity[choice]
+                except KeyError:
+                    print("\nThere is no such key on this level\n")
+                    continue
+            break
+
+    if type(entity) == list:
+        choice = int(choice)
+    check = type(entity[choice])
+    if check == list or check == dict:
+        command = navigation(entity[choice])
+        if command:
+            return navigation(entity)
+    else:
+        print(entity[choice])
 
 
 def main():
@@ -50,5 +80,5 @@ def main():
     navigation(data)
 
 
-
-main()
+if __name__ == "__main__":
+    main()
